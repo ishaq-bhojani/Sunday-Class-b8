55625fca1c6863b3bea21b3793715ad2b37b9173
Ishaq Bhojani <ishaqbhojani@gmail.com>
2022-06-07 00:32:27 +0500

    Class code

---
+++ 29-5-2022/app.js
@@ -0,0 +1,40 @@
+// alert("Hello World");
+        
+// alert("Hello User");
+
+// STRING
+// "ABCD!@#" "test"
+
+// NUMBER
+// 5 8 1 0 -1 -22 0.005
+
+// BOOLEAN
+// true false
+
+// var greeting;
+// greeting = "Hello World";
+
+// var greeting = "Hello World";
+// var num = 24.5;
+// var bool = false;
+// alert(bool);
+
+
+// ILLEGAL NAMES
+// var hello world = "Hello World";
+
+// LEGAL NAMES
+// var hello_world = "Hello World"; // ("UNDERSCORE_CASE")
+// var helloWorld = "Hello World"; // ("CAMEL_CASE")
+
+// where is my bag
+// whereIsMyBag
+
+var num = 5;
+num = num + 5;
+var num2 = 10;
+//  BODMAS
+// alert(5 / (num + num2) * 2); // 20
+
+
+alert(11 % 7);
---
+++ 5-6-2022/app.js
@@ -0,0 +1,35 @@
+// var num = 5;
+// alert(num--);
+// alert(num);
+
+// var num = 5;
+// ++num; // 6
+// var num2 = num--; // 6
+// alert(++num + num2); //12
+
+// var num = 5;
+// var text = "Hello";
+// var text2 = 5;
+// // 5+5+5
+// console.log( text2 + num + text);
+
+// var num = +prompt("What is your fav number?", 5);
+// console.log(num + 5);
+
+// COMPARISON OPERATORS
+
+// var num = 5;
+// var num2 = 10;
+
+// "5" == 5 // true;
+// "5" === 5 // false;
+ 
+// console.log(num !== num2);
+
+var num = +prompt("What is your fav number?", 5);
+
+if (num % 2 === 0) {
+    console.log("This is an even number");
+} else {
+    console.log("This is an odd number");
+}

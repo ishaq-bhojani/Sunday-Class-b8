ad72203cbdd17193477951d6208c40db880e3b6a
Ishaq Bhojani <ishaqbhojani@gmail.com>
2022-06-15 02:26:15 +0500

    12-6-2022 class code

---
+++ 12-6-2022/app.js
@@ -0,0 +1,42 @@
+// var num = +prompt("Type a number"); // 5
+
+// if (num % 3 === 0 && num % 5 === 0) {
+//     console.log("Both 3 & 5");
+// }  else if (num % 3 === 0) {
+//     console.log("Only 3");
+// } else if (num % 5 === 0) {
+//     console.log("Only 5");
+// }
+
+// console.log("undefined" && "true");
+// console.log(null || undefined);
+
+// var num = +prompt("Type your percentage");
+// var grade;
+
+// if (num >= 0 && num <= 100) {
+//   if (num >= 80) {
+//     grade = "A+";
+//   } else if (num >= 70) {
+//     grade = "A";
+//   } else if (num >= 60) {
+//     grade = "B";
+//   } else if (num >= 50) {
+//     grade = "C";
+//   } else {
+//     grade = "D";
+//   }
+//   console.log(grade);
+// } else {
+//     console.log("Wrong input");
+// }
+
+var months = ["January", "February", "April", 4];
+// months[2] = "March";
+// months.push("March");
+// months.pop();
+// months.unshift(1);
+// months.shift();
+// months.splice(2, 1, "March", "April");
+// console.log(months);
+// var months2 = months.slice(0, 4);

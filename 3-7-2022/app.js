e00269473c3c5625746de1572ebdd065c68a142b
Ishaq Bhojani <ishaqbhojani@gmail.com>
2022-07-03 22:35:14 +0500

    3-7-2022 class code

---
+++ 3-7-2022/app.js
@@ -0,0 +1,47 @@
+// var date = new Date();
+// date.getHours() + " : " + date.getMinutes();
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// dayNames[date.getDay()];
+
+// var date2 = new Date();
+// date2.getTime() - date.getTime();
+
+// function tellTime() {
+//   var now = new Date();
+//   var theHr = now.getHours();
+//   var theMin = now.getMinutes();
+//   alert("Current time: " + theHr + ":" + theMin);
+// }
+// tellTime();
+
+// function add(x, y) {
+//    return x + y;
+// }
+
+// var result = add(5, 3);
+// alert(result);
+
+// HOISTING
+// var abc = 10;
+
+// function demo() {
+//     console.log(abc);
+//     abc = 20;
+// }
+// demo();
+
+var dayOfWk = "Fri"
+switch (dayOfWk) {
+  case "Sat":
+    alert("Whoopee Sat");
+    break;
+  case "Sun":
+    alert("Whoopee Sun");
+    break;
+  case :
+    alert("TGIF!");
+    break;
+  default:
+    alert("Shoot me now!");
+}

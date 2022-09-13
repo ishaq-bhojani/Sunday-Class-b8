7ce72a1d4d137e02e162110ec5ea2ca91b31bd2a
Ishaq Bhojani <ishaqbhojani@gmail.com>
2022-09-14 01:28:29 +0500

    11-9-2022 class code

---
+++ 11-9-2022/app.js
@@ -0,0 +1,42 @@
+// function work1 (a, b) {
+//     return a + b;
+// }
+
+// const work2 = (a, b) => a + b;
+
+// console.log(work1(5, 5));
+// console.log(work2(5, 5));
+
+// const evens = [2,4,6,8,10];
+
+// const odd = evens.map(function(val) {
+//     return val - 1;
+// });
+
+// const odd = evens.map(val => val -1);
+
+// console.log(odd);
+
+// const numbers = [1,2,4,6,7,8,10];
+// const evens = numbers.filter(val => val % 2 === 0);
+// console.log(evens);
+
+// evens.forEach(val => console.log(val));
+
+// function add (x, y, ...a) {
+//     let answer = 0;
+//     a.forEach(val => {
+//         answer += val;
+//     });
+//     return x + y + answer;
+// }
+//     console.log(add(1, 2, 7 , 10, 50));
+
+// var params = [ "hello", true, 7 ];
+// var other = [ 1, 2, ...params ];
+// var str = "foo"
+// var chars = [ ...str ]
+
+// console.log(other);
+
+

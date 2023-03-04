34be10e7d326b57178e66a81bf58c2748fd0729a
Ishaq Bhojani <ishaqbhojani@gmail.com>
2023-03-04 22:05:56 +0500

    26-2-2023 class code

---
+++ 26-2-2023/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express()
+const form = require("./routes/form");
+const todo = require("./routes/todo");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use((req, res, next) => { //  Auth Middleware
+    req.data = "Ishaq";
+    next();
+});
+
+app.use("/todo", todo);
+app.use("/form", form);
+
+app.listen(3000);
